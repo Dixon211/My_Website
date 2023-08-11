ec3643548466c7d7bcec2e153fe3dd59c57b7a72
Dixon211 <131546168+Dixon211@users.noreply.github.com>
2023-08-11 15:30:37 -0400

    got left mostly done
    Might need to turn left into an element and add functions

--- my_website/src/App.js
+++ my_website/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import headshot from './headshot.jpg';
+import git_hub from './git_hub_logo.png';
+import linkedin from './linkedin.png';
 
 function App() {
+  
   return (
     <div className='main'>
       <div className='left'>
@@ -9,6 +12,20 @@ function App() {
         <div className='description_div'>
           <label className="name">Michael Dixon</label>
           <label className='description'>It Professional, Coding Enthusiast, Part-time Wizard</label>
+          <div className='sb_btn_div'>
+            <button className='sb_btn'>About Me</button>
+            <button className='sb_btn'>Resume</button>
+            <button className='sb_btn'>Projects</button>
+          </div>
+          <div className='socials'>
+            <button className='social_btn'>
+              <img src={git_hub} className='social_pic' />
+            </button>
+            <button className='social_btn'>
+                <img onClick={() => openInNewTab("https://plainenglish.io") src={linkedin} className='social_pic' />
+            </button>
+            <label className='email'>michael.s.dixon@gmail.com</label>
+          </div>
         </div>
         <div className='btndiv'>
           
