6efdb9b4310eed0bcfd9b84b1bc1bc4a5cf7bfcd
Dixon211 <131546168+Dixon211@users.noreply.github.com>
2023-08-15 11:03:38 -0400

    Resume
    Managed to get the scroll working for right and finished resume

--- my_website/src/Resume.js
+++ my_website/src/Resume.js
@@ -15,7 +15,23 @@ function Resume_section() {
                 <label className='resume_info'>Location: Kensington, MD/Remote</label>
                 <p>{resume_text.Shipshape_desc}</p>
             </div>
-            <div className='resume_entry'>
+            <div className='skills_section'>
+                <div className='skills_header'>
+                    <label className='skills_label'>Skills learned:</label>
+            </div>
+            <div className='skills_header'>
+                <ul className='skills_list'>
+                    <li>AD/Azure AD admin</li>
+                    <li>Mailflow Control</li>
+                    <li>Powershell</li>
+                    <li>Firewall Configuration</li>
+                    <li>Python</li>
+                    <li>Javascript, CSS, HTML</li>
+                    <li>React</li>
+                </ul>
+            </div>
+        </div>
+        <div className='resume_entry'>
             <div className='resume_header_background'>
                 <label className='resume_label'>Food and Drug Administration Contractor(Apex Systems and Symposit LLC)</label>
                 <label className='resume_sublabel'>Tier 1 Helpdesk/Computer Refresh</label>
@@ -26,8 +42,24 @@ function Resume_section() {
                 <label className='resume_info'>Location: Kensington, MD</label>
                 <p>{resume_text.FDA_desc}</p>
             </div>
+            <div className='skills_section'>
+                <div className='skills_header'>
+                    <label className='skills_label'>Skills learned:</label>
+                </div>
+                <div className='skills_header'>
+                    <ul className='skills_list'>
+                        <li>Local Machine Troubleshooting</li>
+                        <li>Email Quarantine</li>
+                        <li>Office365</li>
+                        <li>User Management</li>
+                        <li>Password Mangement</li>
+                        <li>VPN/Network Troubleshooting</li>
+                        <li>Mac Troubleshooting</li>
+                    </ul>
+                </div>
             </div>
-            <div className='resume_entry'>
+        </div>
+        <div className='resume_entry'>
             <div className='resume_header_background'>
                 <label className='resume_label'>EF English First</label>
                 <label className='resume_sublabel'>English Teacher</label>
@@ -38,6 +70,22 @@ function Resume_section() {
                 <label className='resume_info'>Location: Suzhou, China</label>
                 <p>{resume_text.EF_desc}</p>
             </div>
+            <div className='skills_section'>
+                    <div className='skills_header'>
+                        <label className='skills_label'>Skills learned:</label>
+                    </div>
+                    <div className='skills_list_div'>
+                        <ul className='skills_list'>
+                            <li>Teacher Scaffolding</li>
+                            <li>Teaching Strategies</li>
+                            <li>Presentation Skills</li>
+                            <li>Lesson Planning</li>
+                            <li>Concept Breakdown</li>
+                            <li>Teaching Assessments</li>
+                            <li>Event Hosting</li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
