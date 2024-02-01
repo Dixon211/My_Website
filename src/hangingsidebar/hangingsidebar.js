ffe6bdab6f9e9e252425d2327669955ac298b597
Michael Dixon <131546168+Dixon211@users.noreply.github.com>
2024-02-01 17:20:58 -0500

    sidebar logic complete
    sidebar logic complete

--- src/hangingsidebar/hangingsidebar.js
+++ src/hangingsidebar/hangingsidebar.js
@@ -4,20 +4,49 @@ import {useState} from "react";
 
 
 function HangingSidebar () {
-    //SUCCESS!!!
-    //webhooks
-    const windowSize = useWindowSize();
     //state, statechange
     const [isExpanded, setisExpanded] = useState(false);
     const toggleisExpanded = () => {
         setisExpanded(!isExpanded);
     }
+    //functions
+    const resumeDownload = () => {
+        const resumeurl= "/MikeDixon.pdf";
+        const link = document.createElement("a");
+        link.href=resumeurl;
+        link.download ="MikeDixon.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
 
 
     return(
-        <div id="border" className={isExpanded ? 'open':'closed'} style={isExpanded ? {top: windowSize.height/3, height: windowSize.height/5, width:(windowSize.width/10)}:{top: windowSize.height/3, height: windowSize.height/5}}>
-            <btn id="handle" className={isExpanded ? 'open':'closed'} style={isExpanded ? {width:windowSize.width/30}: {width:windowSize.width/30}} onClick={toggleisExpanded}>Press</btn>
-            <div id="drawer" className={isExpanded ? 'open':'closed'}>
+        <div id='sbcont' className={isExpanded ? 'open': 'closed'}>
+            <button id='handle' className={isExpanded ? 'open': 'closed'} onClick={toggleisExpanded}>
+                <label id="handletxt">Contacts</label>
+            </button>
+            <div id='drawer' className={isExpanded ? 'open': 'closed'}>
+                <a className="draweratag" href='#'>
+                    <button className="drawerbutton" onClick={resumeDownload}>
+                        <img src="/resume.png" />
+                    </button>
+                </a>
+                <a className="draweratag" href="mailto:michael.s.dixon211@gmail.com">
+                    <button className="drawerbutton">
+                        <img src="/emailicon.png" />
+                    </button>
+                </a>
+                <a className="draweratag" href="https://github.com/Dixon211" target="_blank" >
+                    <button className="drawerbutton">
+                        <img src="/github-mark.png" />
+                    </button>
+                </a>
+                <a className="draweratag" href="https://www.linkedin.com/in/michael-dixon-2a0ba7b1/" target="_blank">
+                    <button className="drawerbutton">
+                        <img src='/linkedin-logo.png' />
+                    </button>
+                </a>
 
             </div>
         </div>
